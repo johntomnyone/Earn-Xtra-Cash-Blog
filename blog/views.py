c053dd5b176620ddd579f029e9a29b7992adfcde
Tombari Nyone <tom.nyone@gmail.com>
2020-04-07 22:12:05 +0100

    Testing

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from .models import Post
 from django.views import generic
 
 class PostList(generic.ListView):
-	queryset = Post.objects.filter(status=1).order_by('-created_on')
+	queryset = Post.objects.all() #filter(status=1).order_by('-created_on')
 	template_name = 'index.html'
 
 
